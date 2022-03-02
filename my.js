0157ce3cda30a76d5f24a221886a270963821edc
Misigomartin <Misigomartin@gmail.com>
2022-03-02 12:15:56 +0300

    add changes to the js file

---
+++ my.js
@@ -0,0 +1,8 @@
+let add = function (number1, number2) {
+    return number1 + number2;
+    }
+    let number1 = parseInt (prompt("Enter the first number:"));
+    let  number2 = parseInt(prompt("Enter the second number:"));
+    let result = add(number1, number2);
+    alert(result);
+    
