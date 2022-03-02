992960404384c7fb4bd17adfb1a72ffeab8518a3
Misigomartin <Misigomartin@gmail.com>
2022-03-02 14:58:24 +0300

    chnages made to the js file

--- my.js
+++ my.js
@@ -1,3 +1,27 @@
+// calculating degree
+// let Conv = pounds =>pounds / 2.2; {
+//     let pounds = parseInt(prompt("Enter your weight in pounds:"));
+//     let result = Conv(pounds);
+//     alert (result);
+// }
+
+// let BMI = function (height, weight) {
+    //     return weight / height;
+    // }
+    // let weight = parseInt (prompt("Enter your weight:"));
+    // let height = parseInt (prompt ("Enter you height"));
+    // let result = BMI(height, weight);
+    // alert(result);
+
+// Calculating BMI using arrow functions
+    // let BMI = (weight , height) => weight / height; {
+    //     let weight = parseInt (prompt("Enter your weight:"));
+    //     let height = parseInt (prompt("Enter your weight:"));
+    //     let result = BMI(weight, height);
+    //     alert(result);
+    // }
+
+
 let add = function (number1, number2) {
     return number1 + number2;
     }
@@ -5,4 +29,36 @@ let add = function (number1, number2) {
     let  number2 = parseInt(prompt("Enter the second number:"));
     let result = add(number1, number2);
     alert(result);
+
+let substract = function(number1, number2) {
+    return number1 - number2;
+}
+   result = substract(number1, number2);
+   alert(result);
+
+let multiply = function(number1, number2) {
+    return number1 * number2;
+}
+result = multiply(number1, number2);
+alert(result);
+
+let division = function (number1, number2) {
+    return number1 / number2;
+}
+result= division(number1, number2);
+alert(result);
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
